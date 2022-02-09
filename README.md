dea1fe5cb45e343750f6835241abe2dcc9cc636f
Zane Bitter <zbitter@redhat.com>
2022-02-09 15:49:33 -0500

    Build an ISO with a custom ignition

--- README.md
+++ README.md
@@ -3,3 +3,12 @@ Fleeting
 
 This is a prototype of a tool to generate an ephemeral OpenShift installation
 image.
+
+Testing
+-------
+
+Put the desired ignition file at `data/test_ignition.ign`.
+
+Run the tool using `go run cmd/main.go`.
+
+The output ISO is written to `output/fleeting.iso`.
--- cmd/main.go
+++ cmd/main.go
@@ -1,9 +1,17 @@
 package main
 
-import "github.com/openshift-agent-team/fleeting/pkg/isosource"
+import (
+	"github.com/openshift-agent-team/fleeting/pkg/imagebuilder"
+	"github.com/openshift-agent-team/fleeting/pkg/isosource"
+)
 
 func main() {
-	_, err := isosource.EnsureIso()
+	baseImage, err := isosource.EnsureIso()
+	if err != nil {
+		panic(err)
+	}
+
+	err = imagebuilder.BuildImage(baseImage)
 	if err != nil {
 		panic(err)
 	}
